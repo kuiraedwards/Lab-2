6fa5b1d0b69da9b462e0e8da51426fab94f0f205
Kuira Edwards <61628854+kuiraedwards@users.noreply.github.com>
2023-04-14 20:19:25 -0400

    Add files via upload

--- lab-2--intro-to-processes/main2.c
+++ lab-2--intro-to-processes/main2.c
@@ -1,52 +1,49 @@
+// Worked with Keerthana Pullela and Jamarri White
+// Tested in repl.it
+// Kuira Edwards @02942519
 #include  <stdio.h>
 #include  <sys/types.h>
-#include  <stdlib.h>
 
 #define   MAX_COUNT  200
 
-void  ChildProcess(void);                /* child process prototype  */
+void  ChildProcess(int);                /* child process prototype  */
 void  ParentProcess(void);               /* parent process prototype */
 
 void  main(void)
 {
      pid_t  pid;
      int i;
-
-     for(i=0; i<2; i++) {
-          pid = fork();
-          if (pid == 0) 
-               ChildProcess();
-          else if(pid < 0)
-               printf("Fork Error! \n");
+     for(i = 0; i < 2; i++) {
+     pid = fork();
+     if (pid == 0) {
+          ChildProcess(i);
      }
-     ParentProcess();
+     else if (pid < 0) {
+          printf("ERROR"); }
 }
-
-void  ChildProcess(void)
+ParentProcess();
+}
+void  ChildProcess(int myid)
 {
-     int   i;
-     int MAX_COUNT;
-     int random;
+     int   num;
+     printf("Child Pid: %d exiting. \n", getpid());
      srand(getpid());
-     MAX_COUNT = (rand() % 25) +1;
-
-     for (i = 0; i <= MAX_COUNT; i++) {
-          printf("Child Pid: %d is going to sleep!\n", getpid());
-          random = rand() % 10;
-          sleep(random + 1);
-     printf("Child Pid: %d is awake!\n Let's find the Parent: %d!\n", getpid(), getpid());
-}
+     num = rand() % 10;
+     sleep(num + 1);
+     printf("Child Pid awake. \nParent location %d?\n", getpid());
      exit(0);
+     /*for (i = 1; i <= MAX_COUNT; i++)
+          printf("   This line is from child, value = %d\n", i);
+     printf("   *** Child process is done ***\n"); */
 }
 
 void  ParentProcess(void)
 {
-     int   i;
-     int status;
-     int cpid;
+     int   i, pid, status;
 
-     for (i = 0; i <= 2; i++) {
-          cpid = wait(&status);
-          printf("Child Pid: %d has completed\n", cpid);
-}
+     for (i = 0; i; i++) {
+          pid = wait(&status);
+          printf("Child Pid: %d has completed\n");
+     }
+
 }
